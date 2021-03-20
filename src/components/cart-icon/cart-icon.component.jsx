c1bb30b0b846ac798b8f67b85882025a37abc7b7
Edel David <salazaredeldavid@gmail.com>
2021-03-20 04:25:48 +0100

    updated shop data with new data, updated cart utils to group items in cart reducer

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -8,7 +8,7 @@ import {ReactComponent as ShoppingIcon} from "../../assets/shopping-bag.svg"
 import "./cart-icon.styles.scss"
 
 const CartIcon = ({toggleCartHidden}) => (
-    <div className="cart-icon" onClick={()=>{ toggleCartHidden(); console.log("click") }}>
+    <div className="cart-icon" onClick={toggleCartHidden}>
         <ShoppingIcon className="shopping-icon"/>
         <span className="item-count">0</span>
     </div>
--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import {connect} from "react-redux" 
+
+import CustomButton from "../custom-button/custom-button.component"
+import {addItem} from "../../redux/cart/cart.actions"
+
 import "./collection-item.styles.scss"
 
-const CollectionItem = ({name, price, imageUrl}) => {
-    console.log(imageUrl)
+const CollectionItem = ({item, addItem}) => {
+    const {name, price, imageUrl} = item
+
     return (
         <div className="collection-item">
             <div 
@@ -15,8 +21,13 @@ const CollectionItem = ({name, price, imageUrl}) => {
                 <span className="name">{name}</span>
                 <span className="price">{price}</span>
             </div>
+            <CustomButton onClick={()=>addItem(item)} inverted>Add to cart</CustomButton>
         </div>
     )
 }
 
-export default CollectionItem
+const mapDispatchToProps = dispatch => ({
+    addItem: item => dispatch(addItem(item))
+  })
+
+export default connect(null, mapDispatchToProps)(CollectionItem)
